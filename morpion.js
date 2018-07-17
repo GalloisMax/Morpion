2d2b51199ca3afc523d0129a3897c5de182a3c7b
GalloisMax <max.gallois@outlook.fr>
2018-07-17 10:07:25 +0200

    jeu fini

--- morpion.js
+++ morpion.js
@@ -1,187 +1,178 @@
-function estValide(button) {
+const isValidated = button => {
   return button.innerHTML.length == 0;
-}
+};
 
-function setSymbol(btn, player) {
+const setSymbol = (btn, player) => {
   btn.classList.add(player.team);
   btn.innerHTML = player.team;
-}
+};
 
-function rechercherVainqueur(pawns, players, tour) {
-  console.log(players[tour].flag == pawns[0].innerHTML);
-  console.log("P0", pawns[0].innerHTML);
-  console.log("P1", pawns[1].innerHTML);
-  console.log("P2", pawns[2].innerHTML);
-
+const searchWinner = (pawns, players, round) => {
   if (
-    pawns[0].innerText == players[tour].team &&
-    pawns[1].innerText == players[tour].team &&
-    pawns[2].innerText == players[tour].team
+    pawns[0].innerText == players[round].team &&
+    pawns[1].innerText == players[round].team &&
+    pawns[2].innerText == players[round].team
   ) {
-    pawns[0].style.backgroundColor = "#9ACD32";
-    pawns[1].style.backgroundColor = "#9ACD32";
-    pawns[2].style.backgroundColor = "#9ACD32";
+    pawns[0].style.backgroundColor = "#ffee00";
+    pawns[1].style.backgroundColor = "#ffee00";
+    pawns[2].style.backgroundColor = "#ffee00";
     return true;
   }
 
   if (
-    pawns[3].innerHTML == players[tour].team &&
-    pawns[4].innerHTML == players[tour].team &&
-    pawns[5].innerHTML == players[tour].team
+    pawns[3].innerHTML == players[round].team &&
+    pawns[4].innerHTML == players[round].team &&
+    pawns[5].innerHTML == players[round].team
   ) {
-    pawns[3].style.backgroundColor = "#9ACD32";
-    pawns[4].style.backgroundColor = "#9ACD32";
-    pawns[5].style.backgroundColor = "#9ACD32";
+    pawns[3].style.backgroundColor = "#ffee00";
+    pawns[4].style.backgroundColor = "#ffee00";
+    pawns[5].style.backgroundColor = "#ffee00";
     return true;
   }
 
   if (
-    pawns[6].innerHTML == players[tour].team &&
-    pawns[7].innerHTML == players[tour].team &&
-    pawns[8].innerHTML == players[tour].team
+    pawns[6].innerHTML == players[round].team &&
+    pawns[7].innerHTML == players[round].team &&
+    pawns[8].innerHTML == players[round].team
   ) {
-    pawns[6].style.backgroundColor = "#9ACD32";
-    pawns[7].style.backgroundColor = "#9ACD32";
-    pawns[8].style.backgroundColor = "#9ACD32";
+    pawns[6].style.backgroundColor = "#ffee00";
+    pawns[7].style.backgroundColor = "#ffee00";
+    pawns[8].style.backgroundColor = "#ffee00";
     return true;
   }
 
   if (
-    pawns[0].innerHTML == players[tour].team &&
-    pawns[3].innerHTML == players[tour].team &&
-    pawns[6].innerHTML == players[tour].team
+    pawns[0].innerHTML == players[round].team &&
+    pawns[3].innerHTML == players[round].team &&
+    pawns[6].innerHTML == players[round].team
   ) {
-    pawns[0].style.backgroundColor = "#9ACD32";
-    pawns[3].style.backgroundColor = "#9ACD32";
-    pawns[6].style.backgroundColor = "#9ACD32";
+    pawns[0].style.backgroundColor = "#ffee00";
+    pawns[3].style.backgroundColor = "#ffee00";
+    pawns[6].style.backgroundColor = "#ffee00";
     return true;
   }
 
   if (
-    pawns[1].innerHTML == players[tour].team &&
-    pawns[4].innerHTML == players[tour].team &&
-    pawns[7].innerHTML == players[tour].team
+    pawns[1].innerHTML == players[round].team &&
+    pawns[4].innerHTML == players[round].team &&
+    pawns[7].innerHTML == players[round].team
   ) {
-    pawns[1].style.backgroundColor = "#9ACD32";
-    pawns[4].style.backgroundColor = "#9ACD32";
-    pawns[7].style.backgroundColor = "#9ACD32";
+    pawns[1].style.backgroundColor = "#ffee00";
+    pawns[4].style.backgroundColor = "#ffee00";
+    pawns[7].style.backgroundColor = "#ffee00";
     return true;
   }
 
   if (
-    pawns[2].innerHTML == players[tour].team &&
-    pawns[5].innerHTML == players[tour].team &&
-    pawns[8].innerHTML == players[tour].team
+    pawns[2].innerHTML == players[round].team &&
+    pawns[5].innerHTML == players[round].team &&
+    pawns[8].innerHTML == players[round].team
   ) {
-    pawns[2].style.backgroundColor = "#9ACD32";
-    pawns[5].style.backgroundColor = "#9ACD32";
-    pawns[8].style.backgroundColor = "#9ACD32";
+    pawns[2].style.backgroundColor = "#ffee00";
+    pawns[5].style.backgroundColor = "#ffee00";
+    pawns[8].style.backgroundColor = "#ffee00";
     return true;
   }
 
   if (
-    pawns[0].innerHTML == players[tour].team &&
-    pawns[4].innerHTML == players[tour].team &&
-    pawns[8].innerHTML == players[tour].team
+    pawns[0].innerHTML == players[round].team &&
+    pawns[4].innerHTML == players[round].team &&
+    pawns[8].innerHTML == players[round].team
   ) {
-    pawns[0].style.backgroundColor = "#9ACD32";
-    pawns[4].style.backgroundColor = "#9ACD32";
-    pawns[8].style.backgroundColor = "#9ACD32";
+    pawns[0].style.backgroundColor = "#ffee00";
+    pawns[4].style.backgroundColor = "#ffee00";
+    pawns[8].style.backgroundColor = "#ffee00";
     return true;
   }
 
   if (
-    pawns[2].innerHTML == players[tour].team &&
-    pawns[4].innerHTML == players[tour].team &&
-    pawns[6].innerHTML == players[tour].team
+    pawns[2].innerHTML == players[round].team &&
+    pawns[4].innerHTML == players[round].team &&
+    pawns[6].innerHTML == players[round].team
   ) {
-    pawns[2].style.backgroundColor = "#9ACD32";
-    pawns[4].style.backgroundColor = "#9ACD32";
-    pawns[6].style.backgroundColor = "#9ACD32";
+    pawns[2].style.backgroundColor = "#ffee00";
+    pawns[4].style.backgroundColor = "#ffee00";
+    pawns[6].style.backgroundColor = "#ffee00";
     return true;
   }
-}
+};
 
-function matchNul(pawns) {
+const Draw = pawns => {
   for (let i = 0, len = pawns.length; i < len; i++) {
     if (pawns[i].innerHTML.length == 0) return false;
   }
 
   return true;
-}
+};
 
-const Afficheur = function(element) {
-  const affichage = element;
+const Display = function(element) {
+  const render = element;
 
-  function setText(message) {
-    affichage.innerHTML = message;
-  }
+  const setText = message => {
+    render.innerHTML = message;
+  };
 
   return { sendMessage: setText };
 };
 
-function main() {
+const main = () => {
   const pawns = document.querySelectorAll("#Jeu button");
-  console.log(pawns);
   const players = [
     {
       flag:
-        "<img class='flag france' src='http://flags.fmcdn.net/data/flags/w580/fr.png'>",
+        "<img class='flag France' src='https://cdn.countryflags.com/thumbs/france/flag-round-250.png'>",
       team: "France"
     },
     {
       flag:
-        "<img class='flag' src='http://flags.fmcdn.net/data/flags/w580/hr.png'>",
+        "<img class='flag Croatie' src='https://cdn.countryflags.com/thumbs/croatia/flag-round-250.png'>",
       team: "Croatie"
     }
   ];
-  let tour = 0;
-  let jeuEstFini = false;
-  const afficheur = new Afficheur(document.querySelector("#StatutJeu"));
-  //déclare une variable ou on integre un operator new pour rajouter une instance de l'objet Display qui retourne
-  afficheur.sendMessage(
+  let round = 0;
+  let endGame = false;
+  const display = new Display(document.querySelector("#StatutJeu"));
+  display.sendMessage(
     "Le jeu peut commencer ! <br /> L'équipe de " +
-      players[tour].team +
-      " c'est votre tour."
+      players[round].team +
+      " à vous d'engagez."
   );
   for (let i = 0; i < pawns.length; i++) {
     pawns[i].addEventListener("click", function() {
-      if (jeuEstFini) return;
-      console.log("this", this);
-      if (!estValide(this)) {
-        afficheur.sendMessage(
+      if (endGame) return;
+      if (!isValidated(this)) {
+        display.sendMessage(
           "Case occupée ! <br />L'équipe de " +
-            players[tour].team +
+            players[round].team +
             " c'est toujours à vous !"
         );
       } else {
-        setSymbol(this, players[tour]);
-        jeuEstFini = rechercherVainqueur(pawns, players, tour);
-        console.log("jeuestfini", jeuEstFini);
-        if (jeuEstFini) {
-          afficheur.sendMessage(
+        setSymbol(this, players[round]);
+        endGame = searchWinner(pawns, players, round);
+        if (endGame) {
+          display.sendMessage(
             "L'équipe de' " +
-              players[tour].team +
+              players[round].team +
               ' a gagné la coupe du monde 2018 ! <br /> <a href="morpion.html">Rejouer</a>'
           );
           return;
         }
 
-        if (matchNul(pawns)) {
-          afficheur.sendMessage(
+        if (Draw(pawns)) {
+          display.sendMessage(
             'Match Nul !<br /> Place aux tirs aux buts <br/> <a href="morpion.html">Rejouer</a>'
           );
           return;
         }
 
-        tour++;
-        tour = tour % 2;
-        afficheur.sendMessage(
-          "L'équipe de " + players[tour].team + " c'est à vous !"
+        round++;
+        round = round % 2;
+        display.sendMessage(
+          "L'équipe de " + players[round].team + " c'est à vous !"
         );
       }
     });
   }
-}
+};
 
 main();
